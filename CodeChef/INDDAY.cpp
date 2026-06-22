612ed42f65ff779e05fc05f8ab6922af8c2af421
abdsehab <abdullahalsehab2017@gmail.com>
2026-06-22 20:53:39 +0600

    Solve SHOHOJ CODING PROBLEM D5

---
+++ CodeChef/INDDAY.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	
+	if(a<=15) cout<<15-a;
+	else cout<<-1;
+
+}
---
+++ CodeChef/P1209.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	
+	if(a<=4) cout<<"YES";
+	else cout<<"NO";
+
+}
