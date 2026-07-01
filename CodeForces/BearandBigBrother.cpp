70aa2f8f676b9f56e11e591be8eedfd71a307786
abdsehab <abdullahalsehab2017@gmail.com>
2026-07-01 20:47:50 +0600

    Solve CF 791A

---
+++ CodeForces/BearandBigBrother.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    int years = 0;
+    cin>>a>>b;
+
+    while(a<=b){
+        a *= 3;  
+        b *= 2; 
+        years++; 
+    } 
+    cout << years << endl;
+    return 0;
+}
